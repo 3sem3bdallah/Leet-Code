f34e083786eb662f321b9842422a79f80e21f9a6
Asem Abdallah <asem5141@gmail.com>
2025-03-21 16:51:20 +0200

    Delete  SQL 50/Employee-Bonus.sql

---  SQL 50/Employee-Bonus.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    Employee.name, Bonus.bonus
-FROM
-    Employee
-        LEFT JOIN
-    Bonus ON Employee.empId = Bonus.empId
-        AND (Bonus.bonus < 1000
-        OR Bonus.bonus IS NULL);
