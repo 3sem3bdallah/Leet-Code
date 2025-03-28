d95702231c2e6f9d3e14720d4cc26cab045237fb
Asem Abdallah <asem5141@gmail.com>
2025-03-28 22:04:45 +0200

    [1633] [Time Beats: 89.73%] - LeetPush

---
+++  SQL 50/Percentage-of-Users-Attended-a-Contest.sql
@@ -0,0 +1,13 @@
+SELECT 
+    r.contest_id,
+    ROUND(COUNT(r.user_id) / (SELECT 
+                    COUNT(DISTINCT user_id)
+                FROM
+                    Users) * 100,
+            2) AS percentage
+FROM
+    Register r
+        JOIN
+    Users u ON r.user_id = u.user_id
+GROUP BY r.contest_id
+ORDER BY percentage DESC , r.contest_id;
