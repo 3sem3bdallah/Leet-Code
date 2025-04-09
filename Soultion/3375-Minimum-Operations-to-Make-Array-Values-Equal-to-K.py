b4de9d7fbf096df613d38062f7b8978041a5d8b7
Asem Abdallah <asem5141@gmail.com>
2025-04-09 20:10:29 +0200

    [3375] [Time Beats: 54.25%] [Memory Beats: 31.94%] - LeetPush

---
+++ Soultion/3375-Minimum-Operations-to-Make-Array-Values-Equal-to-K.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        unique_nums = set(nums)
+        res = 0
+
+        for num in unique_nums:
+            if num > k:
+                res += 1
+            if num < k:
+                return -1
+
+        return res
+        
