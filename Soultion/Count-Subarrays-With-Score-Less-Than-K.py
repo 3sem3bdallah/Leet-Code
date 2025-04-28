901a199aa7fa7ecae0b0a7fb6002cb95fd7b745e
Asem Abdallah <asem5141@gmail.com>
2025-04-28 23:36:35 +0300

    [2302] [Time Beats: 61.55%] [Memory Beats: 97.85%] - LeetPush

---
+++ Soultion/Count-Subarrays-With-Score-Less-Than-K.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        count = 0
+        left = 0
+        current_sum = 0
+        
+        for right in range(n):
+            current_sum += nums[right]
+            current_length = right - left + 1
+            
+            while current_sum * current_length >= k and left <= right:
+                current_sum -= nums[left]
+                left += 1
+                current_length = right - left + 1
+            
+            count += right - left + 1
+        
+        return count
