393090abc49af679a15f763d7a36ccfd7f0b3391
Asem Abdallah <asem5141@gmail.com>
2025-06-15 04:21:56 +0300

    [1432] [Time Beats: 100.00%] [Memory Beats: 34.69%] - LeetPush

---
+++ Soultion/Max-Difference-You-Can-Get-From-Changing-an-Integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxDiff(self, num: int) -> int:
+        s = str(num)
+
+        match = re.search(r'[^9]', s)
+        maxDigit = match.group() if match else None
+        max = int(s.replace(maxDigit, '9')) if maxDigit else num
+
+        if s[0] != '1':
+            min = int(s.replace(s[0], '1'))
+        else:
+            match2 = re.search(r'[2-9]', s)
+            minDigit = match2.group() if match2 else None
+            min = int(s.replace(minDigit, '0')) if minDigit else num
+
+        return max - min
