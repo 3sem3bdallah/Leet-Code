9852dac2102642b06832608e109080c4ef991137
Asem Abdallah <asem5141@gmail.com>
2026-05-31 05:21:40 +0300

    [2126] [Time Beats: 81.52%] [Memory Beats: 65.13%] - LeetPush

---
+++ Soultion/Destroying-Asteroids.java
@@ -0,0 +1,17 @@
+1class Solution {
+2    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+3        Arrays.sort(asteroids);
+4
+5        long currentMass = mass;
+6
+7        for (int asteroid : asteroids) {
+8            if (currentMass < asteroid) {
+9                return false;
+10            }
+11
+12            currentMass += asteroid;
+13        }
+14
+15        return true;
+16    }
+17}
